7a1a39bbfa4e50f2a2d5c883ffb7ed53b5200d4f
mystry112000 <mystry112000@gmail.com>
2026-05-20 01:26:23 +0530

    Fix SW: scope-relative paths, cache-first for PWA launch

--- sw.js
+++ sw.js
@@ -1,18 +1,22 @@
-const CACHE = 'love-letters-v1';
-const URLS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'love-letters-v2';
+const FILES = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(ks => Promise.all(ks.map(k => k !== CACHE && caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(ks.map(k => k !== CACHE && caches.delete(k))))
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    fetch(e.request)
+      .then(r => (caches.open(CACHE).then(c => c.put(e.request, r.clone())), r))
+      .catch(() => caches.match(e.request).then(m => m || caches.match('./index.html')))
   );
 });
